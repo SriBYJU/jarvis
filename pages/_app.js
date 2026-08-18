9181132c5c0a587a21310528a4cf973587345273
SriBYJU <95580233+SriBYJU@users.noreply.github.com>
2026-08-18 16:32:45 -0400

    Wire full Jarvis UI into v6 runtime

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/globals.css';
+import '../styles/jarvis-v6.css';
+import FastMapRuntime from '../components/FastMapRuntime';
+import JarvisRuntimeV6 from '../components/JarvisRuntimeV6';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+    <FastMapRuntime />
+    <Component {...pageProps} />
+    <JarvisRuntimeV6 />
+  </>;
 }
