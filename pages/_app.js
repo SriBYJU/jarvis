9e590c349d1dbf7b050fdf7b79326b96679ec0d6
SriBYJU <95580233+SriBYJU@users.noreply.github.com>
2026-08-18 16:25:42 -0400

    Restore full original Jarvis home experience

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,5 @@
-import { useRouter } from 'next/router';
 import '../styles/globals.css';
-import '../styles/jarvis-v4.css';
-import '../styles/jarvis-v5.css';
-import FastMapRuntime from '../components/FastMapRuntime';
-import JarvisShellV5 from '../components/JarvisShellV5';
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-  if (router.pathname === '/') {
-    return <>
-      <FastMapRuntime />
-      <JarvisShellV5 />
-    </>;
-  }
   return <Component {...pageProps} />;
 }
