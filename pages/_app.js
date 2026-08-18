58a22c24d91811e5394a3c5edb58c98f3f7b24c4
SriBYJU <95580233+SriBYJU@users.noreply.github.com>
2026-08-18 14:52:12 -0400

    mount Jarvis 3 local core bridge

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
 import '../styles/globals.css';
+import '../styles/jarvis-core.css';
+import JarvisCoreOverlay from '../components/JarvisCoreOverlay';
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+    <Component {...pageProps} />
+    <JarvisCoreOverlay />
+  </>;
 }
