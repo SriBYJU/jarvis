8e3d24ff191f067f64d048c524f85cde8de1f025
SriBYJU <95580233+SriBYJU@users.noreply.github.com>
2026-08-18 16:09:36 -0400

    Use bundled fast map runtime

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
 import '../styles/jarvis-v4.css';
+import FastMapRuntime from '../components/FastMapRuntime';
 import JarvisExperienceV4 from '../components/JarvisExperienceV4';
 
 export default function App({ Component, pageProps }) {
   return <>
     <Component {...pageProps} />
+    <FastMapRuntime />
     <JarvisExperienceV4 />
   </>;
 }
