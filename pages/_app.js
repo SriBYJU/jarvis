edf5df14611a99e17d021ca5ec6b09507cf099e1
SriBYJU <95580233+SriBYJU@users.noreply.github.com>
2026-08-18 15:00:04 -0400

    mount Spotify command bridge

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
 import '../styles/jarvis-core.css';
 import JarvisCoreOverlay from '../components/JarvisCoreOverlay';
+import SpotifyCommandBridge from '../components/SpotifyCommandBridge';
 
 export default function App({ Component, pageProps }) {
   return <>
     <Component {...pageProps} />
     <JarvisCoreOverlay />
+    <SpotifyCommandBridge />
   </>;
 }
